02eebe472c058428b5e8740256dd105194163e2e
Sy <syeung@mail.usf.edu>
2018-04-03 00:00:27 -0400

    Fix minor starship controller syntax

--- controllers/starshipController.js
+++ controllers/starshipController.js
@@ -1,6 +1,7 @@
 var Starship = require('../models/starship');
 
 // Handle Starship create on POST.
-esxport.starship_create_post = function(req, res) {
+exports.starship_create_post = function(req, res) {
     res.send('TEST POST');
-}
+};
+
